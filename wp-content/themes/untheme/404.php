5e7448ca920b82bed7ccf823b155f4da767ee0de
aleksandra <aleksandra.ovsia@gmail.com>
2024-10-05 00:40:26 +0300

    eds

--- wp-content/themes/untheme/404.php
+++ wp-content/themes/untheme/404.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying 404 pages (not found)
  *
@@ -10,51 +11,56 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
+	<section class="page-section error-404 not-found">
+		<div class="fixed-container">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'untheme' ); ?></h1>
+				<ul class="breadcrumbs__list">
+					<?php echo site_breadcrumbs(); ?>
+				</ul>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'untheme' ); ?></p>
+			<h4 class="page-404-title"><?php esc_html_e('Такой страницы нет', 'untheme'); ?></h4>
+				<p>404</p>
 
+				<div class="search-content">
+					<p>Вы можете перейти на <a href="<?php echo site_url() ?>">главную страницу</a>
+						или&nbsp;воспользоваться поиском по&nbsp;сайту</p>
 					<?php
 					get_search_form();
 
-					the_widget( 'WP_Widget_Recent_Posts' );
+					//the_widget( 'WP_Widget_Recent_Posts' );
 					?>
+				</div>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'untheme' ); ?></h2>
+
+
+				<!-- <div class="widget widget_categories"> -->
+				<!-- <h2 class="widget-title"><?php //esc_html_e( 'Most Used Categories', 'untheme' ); 
+												?></h2>
 						<ul>
 							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
+							// wp_list_categories(
+							// 	array(
+							// 		'orderby'    => 'count',
+							// 		'order'      => 'DESC',
+							// 		'show_count' => 1,
+							// 		'title_li'   => '',
+							// 		'number'     => 10,
+							// 	)
+							// );
 							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$untheme_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'untheme' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$untheme_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+						</ul> -->
+				<!-- </div> -->
+				<!-- .widget -->
 
 			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
+		</div>
+	</section><!-- .error-404 -->
 
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
 get_footer();
