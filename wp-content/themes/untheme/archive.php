9627d92e8999af352cab91cf8cbf926c07ca3218
aleksandra <aleksandra.ovsia@gmail.com>
2024-10-04 10:18:36 +0300

    eds

--- wp-content/themes/untheme/archive.php
+++ wp-content/themes/untheme/archive.php
@@ -11,14 +11,17 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
+	<section class="page-section">
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<?php
+				<div class="fixed-container">
+					<?php
 				the_archive_title( '<h1 class="page-title">', '</h1>' );
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
+				</div>
+				
 			</header><!-- .page-header -->
 
 			<?php
@@ -43,7 +46,7 @@ get_header();
 
 		endif;
 		?>
-
+	</section>
 	</main><!-- #main -->
 
 <?php
