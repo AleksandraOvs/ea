fffacdf86b9f537a15c7baba05f1139826525771
aleksandra <aleksandra.ovsia@gmail.com>
2024-10-10 22:14:49 +0300

    eds

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'eventdecor_db' );
+define( 'DB_NAME', 'ea' );
 
 /** Database username */
-define( 'DB_USER', 'eventdecor_db' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '&b3pVwRO38q1' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'MySQL-8.0' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
